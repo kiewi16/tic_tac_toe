88aa68518f98036cb32df91b42300b18c1e9b60b
Kim Ewing <pekar.kimberly@gmail.com>
2024-04-19 13:31:11 -0600

    adds function updateHeaderWithWinner and updateHeaderWithDraw functions

--- main.js
+++ main.js
@@ -14,26 +14,19 @@ var winConditions = [
 ]
 
 // query selectors 
-
-// var board = document.querySelector('.tic-tac-toe-board')
 var boardSquares = document.querySelectorAll('.square')
 var mainHeader = document.querySelector('h2')
 var rainbowWins = document.querySelector('.rainbow-wins')
 var unicornWins = document.querySelector('.unicorn-wins')
 
 // event listeners
-
 for (var i = 0; i < boardSquares.length; i++) {
     boardSquares[i].addEventListener('click', checkSquareStatus)
 }
-
 window.addEventListener('load', createPlayers)
 
 // functions
-
 // A function that creates the objects that store each players’ information - properties should include: id (ex: 'one'), token (ex: '⭐️'), wins (ex: 0)
-
-// add parameter which is the name of the player, basd on the name of the player that I am passing as an argument to my function invocation, I will return the player object. 
 
 function createPlayers (token) {
 if (token === "🌈") { 
@@ -60,7 +53,7 @@ if (token === "🌈") {
 function checkSquareStatus(event) {
     var selectedSquare = event.target.closest('div')
     var selectedSquareIndex = parseInt(selectedSquare.getAttribute("cellIndex")) 
-    updateSquare (selectedSquare, selectedSquareIndex)
+    updateSquare(selectedSquare, selectedSquareIndex)
 }
     
 function updateSquare (selectedSquare, selectedSquareIndex) {
@@ -71,7 +64,7 @@ function updateSquare (selectedSquare, selectedSquareIndex) {
       var currentPlayerToken = squareStatus[selectedSquareIndex]
       shouldSwitchTurn = true
       shouldMakeMove = true 
-      updateBoardToken (currentPlayerToken, selectedSquare)
+      updateBoardToken(currentPlayerToken, selectedSquare)
     } else if (squareStatus[selectedSquareIndex] === "🌈" || squareStatus[selectedSquareIndex] === "🦄") {
       window.alert('Please Select Another Square!')
     }
@@ -82,10 +75,11 @@ function updateSquare (selectedSquare, selectedSquareIndex) {
     var player = player1.isTurn ? player1 : player2
     checkWinConditions(player)
     checkForDraw(player1, player2)
-    
+
     if (shouldSwitchTurn) {
       switchPlayersTurn(player1, player2)
     }
+
 }
 
 function updatePlayerMoves (selectedSquareIndex) {
@@ -98,7 +92,6 @@ function updatePlayerMoves (selectedSquareIndex) {
 }
 
 // A function that keeps track of which player’s turn it currently is
-// selectedSquareIndex is the parameter
 // I am updating the player.moves in the makeMove function. The moves need to be updated before the checkWinConditions function is invoked, otherwise the checkWinConditions function will not be evaluating the most recent player.moves to the winConditions array. 
 
 function switchPlayersTurn(player1, player2) {
@@ -129,8 +122,7 @@ function checkWinConditions(player) {
             console.log(player.id === 1 ? "Player 1 Wins!" : "Player 2 Wins!")
             increaseWins(playerSquaresTowardsWinCounter, player)
             updateHeaderWithWinner(player)
-            setTimeout(resetGame, 3000)
-            return
+            setTimeout(resetGame, 1000)
         }
     }
     return playerSquaresTowardsWinCounter
@@ -144,7 +136,7 @@ function checkForDraw(player1, player2, playerSquaresTowardsWinCounter) {
     if (totalPlayerMoves === totalBoardSquares && playerSquaresTowardsWinCounter !== 3){
         console.log("This Game is a Draw")
         updateHeaderWithDraw()
-        setTimeout(resetGame, 3000)
+        setTimeout(resetGame, 1000)
         return true; 
     } else {console.log("this game is not a draw")}
 }
@@ -181,12 +173,13 @@ function resetGame() {
 
 // functions that update the DOM 
 function updateBoardToken (currentPlayerToken, selectedSquare) {
-    selectedSquare.innerText = currentPlayerToken
+    selectedSquare.innerText = `${currentPlayerToken}`
 }
 
 function updateHeader(currentPlayerToken) {
     mainHeader.innerText = `It's ${currentPlayerToken}'s Turn`
 }
+
 
 function updatePlayerWins(player) {
     var playerWins
@@ -200,10 +193,19 @@ function updatePlayerWins(player) {
 
 function updateHeaderWithWinner(player) {
     mainHeader.innerText = `${player.token} Won`
-    console.log("line 202:", mainHeader.innerText)
+    setTimeout(() => {
+        mainHeader.innerText = `${player.token} Won!`;
+    }, 0)
 }
 
+// function updateHeaderWithDraw() {
+//     mainHeader.innerText = `This Game is a Draw!`
+//     console.log("linke 207:", mainHeader.innerText)
+// }
+
 function updateHeaderWithDraw() {
-    mainHeader.innerText = `This Game is a Draw!`
-    console.log("linke 207:", mainHeader.innerText)
+    mainHeader.innerText = `This Game is a Draw!`;
+    setTimeout(() => {
+        mainHeader.innerText = `This Game is a Draw!`;
+    }, 0);
 }
